933a87c5713b5ad2ca7f452d15f486fe74bc20fe
Code Ape <fardin.barashi@gmail.com>
2026-05-20 20:59:36 +0200

    Add files via upload

---
+++ TestFiles/example.cmd
@@ -0,0 +1,3 @@
+@echo off
+echo Detta ar ett test i CMD
+echo lite andra ord
---
+++ TestFiles/example.psm1
@@ -0,0 +1,4 @@
+function Get-TestMessage {
+    return "Detta är ett test i PSM1-modul"
+}
+Export-ModuleMember -Function Get-TestMessage
